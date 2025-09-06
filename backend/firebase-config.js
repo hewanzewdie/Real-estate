59330f11b1c1037b70d45ff9421e0d2ac2a6b37d
hewanzewdie <hewanzewdie16@gmail.com>
2025-09-06 22:27:51 +0300

    initial backend setup

---
+++ backend/firebase-config.js
@@ -0,0 +1,18 @@
+const { initializeApp } = require('firebase/app');
+const { getFirestore } = require('firebase/firestore');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+module.exports = { db };
