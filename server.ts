683d9a4e53333f3a58f23a02852dbf78ab425790
hewan <hewanzewdie16@gmail.com>
2025-11-25 19:23:10 +0300

    cors

--- server.ts
+++ server.ts
@@ -5,7 +5,7 @@ import cors from 'cors';
 const app = express();
 
 app.use(cors({
-  origin: "https://real-estate-2-dun.vercel.app/",
+  origin: "https://real-estate-2-dun.vercel.app",
   methods: "GET,POST,PUT,DELETE",
   allowedHeaders: "Content-Type, Authorization"
 }));
@@ -17,5 +17,9 @@ app.get('/', (req, res) => {
 });
 
 app.use(router);
+
+if (process.env.NODE_ENV !== "production") {
+  app.listen(3000, () => console.log("Local server running on port 3000"));
+}
 
 export default app;
